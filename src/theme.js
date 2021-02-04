9cc8d3ea9a7f8044d16070cef6226ca3194af73b
smolfly <68849295+smolfly@users.noreply.github.com>
2021-02-04 23:39:33 +0300

    Вынесение в функции

--- src/theme.js
+++ src/theme.js
@@ -1,38 +1,62 @@
+
+// У вас дублируется код, чтобы избавиться от дублирования кода выносите его в функции 
+
+// Вот эти строки нужно вынести в функцию 
+// refs.themeSwitchToggle.removeAttribute('checked');
+// refs.body.classList.replace(Theme.DARK, Theme.LIGHT);
+// localStorage.setItem('theme', JSON.stringify(Theme.LIGHT));
+
+// // а так же эти 
+
+// refs.themeSwitchToggle.setAttribute('checked', 'true');
+// refs.body.classList.add(Theme.DARK);
+
+
 const Theme = {
-    LIGHT: 'light-theme',
-    DARK: 'dark-theme',
-  };
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const refs = {
+  themeSwitchToggle: document.querySelector('#theme-switch-toggle'),
+  body: document.querySelector('body'),
+};
 
 
-  const refs = {
-    themeSwitchToggle: document.querySelector('#theme-switch-toggle'),
-    body: document.querySelector('body'),
-  };
+refs.body.classList.add(Theme.LIGHT);
+const savedTheme = localStorage.getItem('theme');
 
+if (savedTheme === JSON.stringify(Theme.DARK)) {
+ toDarkTheme();
+} else {
+  toLigthTheme();
+}
 
-  refs.body.classList.add(Theme.LIGHT);
-  const savedTheme = localStorage.getItem('theme');
-
-  if (savedTheme === JSON.stringify(Theme.DARK)) {
-    refs.themeSwitchToggle.setAttribute('checked', 'true');
-    refs.body.classList.add(Theme.DARK);
+function toSwitchTheme() {
+  if (refs.themeSwitchToggle.getAttribute('checked')) {
+    toLigthTheme();
+  
+ 
   } else {
-    refs.themeSwitchToggle.removeAttribute('checked');
-    refs.body.classList.add(Theme.LIGHT);
+    toDarkTheme();
+  
+  }
+}
+
+
+function toLigthTheme() {
+  refs.themeSwitchToggle.removeAttribute('checked');
+  refs.body.classList.replace(Theme.DARK, Theme.LIGHT);
+  localStorage.setItem('theme', JSON.stringify(Theme.LIGHT));
   }
   
-  function toSwitchTheme() {
-    if (refs.themeSwitchToggle.getAttribute('checked')) {
-      refs.themeSwitchToggle.removeAttribute('checked');
-      refs.body.classList.replace(Theme.DARK, Theme.LIGHT);
-      localStorage.setItem('theme', JSON.stringify(Theme.LIGHT));
-    } else {
-      refs.themeSwitchToggle.getAttribute('checked', 'true');
-      refs.body.classList.replace(Theme.LIGHT, Theme.DARK);
-      localStorage.setItem('theme', JSON.stringify(Theme.DARK));
-    }
+  function toDarkTheme() {
+  refs.themeSwitchToggle.setAttribute('checked', 'true');
+  refs.body.classList.add(Theme.DARK);
+  localStorage.setItem('theme', JSON.stringify(Theme.DARK));
   }
-  
-  refs.themeSwitchToggle.addEventListener('change', toSwitchTheme);
+
+refs.themeSwitchToggle.addEventListener('change', toSwitchTheme);
+
+
 
 
